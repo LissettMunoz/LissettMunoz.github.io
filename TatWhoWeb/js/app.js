2c2b3e03138c01a24bbc64917eb128a5dc5d51a4
Lissett M <31522832+LissettMunoz@users.noreply.github.com>
2018-04-08 22:22:40 -0400

    Tat-Who's Page

---
+++ TatWhoWeb/js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// angular.js main app initialization
+var app = angular.module('tatwho', []).
+    config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+        when('/', {
+          templateUrl: 'pages/index.html'
+        }).
+        when('/home', {
+          templateUrl: 'pages/home.html'
+        }).
+        when('/userhome', {
+          templateUrl: 'pages/userhome.html'
+        }).
+        when('/usersettings', {
+          templateUrl: 'pages/usersettings.html'
+        }).
+        when('/artistsettings', {
+          templateUrl: 'pages/artistsettings.html'
+        }).
+        when('/artistedit', {
+          templateUrl: 'pages/artistedit.html'
+        }).
+        when('/shopsettings', {
+          templateUrl: 'pages/shopsettings.html'
+        }).
+        when('/search', {
+          templateUrl: 'pages/search.html'
+        }).
+        when('/searchresults', {
+          templateUrl: 'pages/dummysearch.html'
+        }).
+        otherwise({ redirectTo: '/' });
+    }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
+  }]);
+
+app.config(['$locationProvider', function($location) {
+    $location.hashPrefix('!');
+}]);
---
+++ TatWhoWeb/js/controllers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// optional controllers
+function HomeCtrl($scope, $http) {
+}
+
+function UserHomeCtrl($scope, $http) {
+}
+
+function UserSettingsCtrl($scope, $http) {
+}
+
+function ArtistSettingsCtrl($scope, $http) {
+}
+
+function ShopSettingsCtrl($scope, $http) {
+}
+
+function SearchPageCtrl($scope, $http) {
+}
+
+function DummySearchPageCtrl($scope, $http) {
+}
